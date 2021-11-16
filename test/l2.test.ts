cefad7fd8c67771255635e836bd3f63e01dd4f0c
geoknee <georgeknee@googlemail.com>
2021-11-16 12:40:55 +0000

    complete test case for proving fraud with identical tickets

--- test/l2.test.ts
+++ test/l2.test.ts
@@ -1,4 +1,6 @@
 import { ethers } from "hardhat";
+const { expect } = require("chai");
+
 import { Contract } from "ethers";
 
 type Ticket = {
@@ -12,17 +14,19 @@ type Ticket = {
 const abiType = [
   "tuple(uint256 nonce,uint256 value,address receiver, address sender, bytes32 escrowHash)",
 ];
-const receiverWallet = new ethers.Wallet(
+const receiverWallet = new ethers.Wallet( // Bob
   "0x91f47a1911c0fd985b34c25962f661f0de606f7ad38ba156902dff48b4d05f97",
   ethers.provider
 );
-const senderWallet = new ethers.Wallet(
+const senderWallet = new ethers.Wallet( // Alice
   "0xf3d5b8ba24833578a22960b2c7a8be1ebb7907ffe0b346111b8839e981b28b0c",
   ethers.provider
 );
 
 const preimage = ethers.utils.hashMessage("Some secret preimage");
-const escrowHash = ethers.utils.keccak256(preimage);
+const escrowHash = ethers.utils.keccak256(
+  ethers.utils.defaultAbiCoder.encode(["bytes32"], [preimage])
+);
 
 let l2Contract: Contract;
 
@@ -31,11 +35,11 @@ describe("L2 Contract", function () {
     const l2Deployer = await ethers.getContractFactory(
       "L2Contract",
       senderWallet
-    );
-    l2Contract = await l2Deployer.deploy();
+    ); // Bob deploys
+    l2Contract = (await l2Deployer.deploy()).connect(receiverWallet); // Alice sends all further txs. She'll pay the gas in this example, and be msg.sender when it counts.
   });
 
-  it("shouldn't allow proof of fraud with identical commiments", async () => {
+  it("shouldn't allow proof of fraud with identical commitments", async () => {
     const ticket: Ticket = {
       nonce: 1,
       value: 5,
@@ -43,6 +47,7 @@ describe("L2 Contract", function () {
       sender: senderWallet.address,
       escrowHash: escrowHash,
     };
+
     const ticketHash = ethers.utils.keccak256(
       ethers.utils.defaultAbiCoder.encode(abiType, [
         [
@@ -55,9 +60,18 @@ describe("L2 Contract", function () {
       ])
     );
 
-    const { r, s, v } = await signData(ticketHash, senderWallet.privateKey);
-    const ticketSignature = ethers.constants.HashZero; // TODO this isn't a real signature
-    await l2Contract.commitToWithdrawal(ticket, ticketSignature);
+    await l2Contract.lockFundsInEscrow(
+      // Alice locks, and escrowHash is written to chain
+      receiverWallet.address,
+      escrowHash,
+      0,
+      0
+    );
+
+    const { r, s, v } = await signData(ticketHash, senderWallet.privateKey); // Bob signs
+    const ticketSignature = { r, s, v };
+
+    await l2Contract.commitToWithdrawal(ticket, ticketSignature); // Bob's commitment written to chain
 
     const commitedTicket = ticket;
     const secondTicket = ticket;
@@ -65,13 +79,16 @@ describe("L2 Contract", function () {
     const secondSignature = ticketSignature;
     const escrowSecret = preimage;
 
-    await l2Contract.proveFraud(
-      commitedTicket,
-      firstSignature,
-      secondTicket,
-      secondSignature,
-      escrowSecret
-    );
+    await expect(
+      l2Contract.proveFraud(
+        // Alice tries to prove Bob's fraud
+        commitedTicket,
+        firstSignature,
+        secondTicket,
+        secondSignature,
+        escrowSecret
+      )
+    ).to.be.revertedWith("tickets must be distinct");
   });
 });
 
