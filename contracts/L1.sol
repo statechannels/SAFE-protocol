6a15fbfcb712ef064da5440b6ebc9167cd0fd2d9
Alex Gap <alex.gap@gmail.com>
2021-11-17 11:17:43 -0800

    Documentation and clean up

--- contracts/L1.sol
+++ contracts/L1.sol
@@ -4,9 +4,12 @@ pragma solidity ^0.8.10;
 import "./common.sol";
 
 contract L1Contract {
+    /// This is a record of the highest nonce per sender.
     mapping(address => uint256) nonces;
+    /// This is a record of funds  allocated to different senders.
     mapping(address => uint256) balances;
 
+    /// Claims multiple tickets.
     function claimTickets(
         WithdrawalTicket[] calldata tickets,
         bytes32[] calldata escrowPreimages,
@@ -17,6 +20,8 @@ contract L1Contract {
         }
     }
 
+    /// Claims a single ticket.
+    /// If the ticket, escrowPreimage, and signature are valid, the funds are transferred to the receiver.
     function claimTicket(
         WithdrawalTicket calldata ticket,
         bytes32 escrowPreimage,
@@ -60,18 +65,7 @@ contract L1Contract {
     }
 
     function deposit() public payable {
+        // TODO: Overflow check?
         balances[msg.sender] += msg.value;
     }
-
-    // Q: How does someone recover their funds to themselves?
-    // A: they can just sign a ticket with themselves as sender and reciever
-    // Q: But this means they can pull their funds out at any time! They aren't truly locked?
-    //    They can send to any other account they control, too?
-    //    HOWEVER: the nonce protects people. The only way to yank the funds is to reuse a nonce.
-    //    Alice will not unlock her payment to Bob on L2 unless she sees Bob commit to them on L2.
-    //    The commitment by Bob on L2 gives Alice protection. If her L1.claimTicket tx reverts with "Ticket nonce must be the next available nonce"
-    //    She must immediately go back to L2 with her ticket, and the fraudulent ticket (previously submitted to L1). She calls L2.proveFraud(ticket1,ticket2) which releases
-    //    her l2 funds back to her.
-    //    The timeouts must protect her, too. It cannot be possible for Bob to get the L2 funds until she has had time to claim
-    //    her L1 funds, spot the failure, and come back to L2 to cancel the payment.
 }
--- contracts/common.sol
+++ contracts/common.sol
@@ -6,10 +6,17 @@ struct Signature {
     bytes32 s;
     uint8 v;
 }
+
+/// This represents a request to move ETH on L1 from sender to receiver.
 struct WithdrawalTicket {
+    /// The amount of value to be transferred.
     uint256 value;
+    /// Every ticket with a sender should have a unique nonce.
     uint256 nonce;
+    /// The address that will receive the funds.
     address payable receiver;
+    /// The address the funds are being sent on behalf of.
     address sender;
+    /// The hash for the funds locked in escrow for this ticket.
     bytes32 escrowHash;
 }
