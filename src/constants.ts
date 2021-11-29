6ad6403018a3438652459b5abe20149c305068c5
Alex Gap <alex.gap@gmail.com>
2021-11-29 13:49:44 -0800

    rename to Alice and Bob

--- src/constants.ts
+++ src/constants.ts
@@ -6,7 +6,7 @@ export const ESCROW_ABI_TYPE = [
   "tuple(address receiver, address sender, uint256 value, uint256 claimStart, uint256 claimExpiry, bytes32 escrowHash)",
 ];
 
-export const RECEIVER_PK =
+export const ALICE_PK =
   "0x91f47a1911c0fd985b34c25962f661f0de606f7ad38ba156902dff48b4d05f97";
-export const SENDER_PK =
+export const BOB_PK =
   "0xf3d5b8ba24833578a22960b2c7a8be1ebb7907ffe0b346111b8839e981b28b0c";
