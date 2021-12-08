0f703217c5e6a8930eee88ba1f6a2fe0a520022a
Alex Gap <alex.gap@gmail.com>
2021-12-08 12:47:26 -0800

    simple gas reporting

--- test/l1.test.ts
+++ test/l1.test.ts
@@ -1,5 +1,5 @@
 import { ethers } from "hardhat";
-import { BigNumber, Contract } from "ethers";
+import { BigNumber } from "ethers";
 
 import {
   ALICE_PK,
@@ -17,6 +17,8 @@ import { L1Contract } from "../contract-types/L1Contract";
 import { L1Contract__factory } from "../contract-types/factories/L1Contract__factory";
 import { TestToken__factory } from "../contract-types/factories/TestToken__factory";
 
+import Table from "cli-table";
+
 const alice = new ethers.Wallet(ALICE_PK, ethers.provider);
 const bob = new ethers.Wallet(BOB_PK, ethers.provider);
 
@@ -24,15 +26,42 @@ const preimage = ethers.utils.hashMessage("Some secret preimage");
 const escrowHash = ethers.utils.keccak256(preimage);
 
 const ticketExpiry = 9_999_999_999;
-const ticketBatchSize = 10;
+
 const amountOfTickets = 100;
+const ticketBatchSizes = [5, 20, 50, 100];
 const ticketValue = 10000;
 const depositValue = ticketValue * amountOfTickets * 100;
 
 let l1Contract: L1Contract;
 let tokenContract: IERC20;
 use(solidity);
+type GasUsed = { gasUsed: BigNumber; claims: number; batchSize: number };
 
+const gasPerScenario: Record<string, GasUsed> = {};
+after(() => {
+  const table = new Table({
+    head: [
+      "Scenario",
+      "Total Gas Used",
+      "Total Claims",
+      "Batch Size",
+      " Average Gas Per Claim",
+    ],
+  });
+  for (const scenario of Object.keys(gasPerScenario)) {
+    const averagePerClaim = gasPerScenario[scenario].gasUsed
+      .div(gasPerScenario[scenario].claims)
+      .toNumber();
+    table.push([
+      scenario,
+      gasPerScenario[scenario].gasUsed,
+      gasPerScenario[scenario].claims,
+      gasPerScenario[scenario].batchSize,
+      averagePerClaim,
+    ]);
+  }
+  console.log(table.toString());
+});
 describe(`L1 Contract using ${USE_ERC20 ? "ERC20 tokens" : "ETH"}`, () => {
   beforeEach(async () => {
     const l1Deployer = new L1Contract__factory(bob);
@@ -69,6 +98,11 @@ describe(`L1 Contract using ${USE_ERC20 ? "ERC20 tokens" : "ETH"}`, () => {
   });
 
   it(`can handle ${amountOfTickets} tickets being claimed sequentially`, async () => {
+    const ticketGasUsage = {
+      gasUsed: BigNumber.from(0),
+      claims: 0,
+      batchSize: 0,
+    };
     const initialBalances = await getBalances(alice, bob, tokenContract);
 
     USE_ERC20
@@ -98,60 +132,16 @@ describe(`L1 Contract using ${USE_ERC20 ? "ERC20 tokens" : "ETH"}`, () => {
     for (let i = 0; i < tickets.length; i++) {
       const { r, s, v } = ticketSignatures[i];
 
-      await l1Contract.claimTicket(tickets[i], preimage, { r, s, v });
-    }
-    const finalBalances = await getBalances(alice, bob, tokenContract);
-
-    const expectedTotalTransferred = BigNumber.from(
-      amountOfTickets * ticketValue
-    );
-    const actualTotalTransferred = finalBalances.alice.sub(
-      initialBalances.alice
-    );
-
-    expect(expectedTotalTransferred.eq(actualTotalTransferred)).to.be.true;
-  });
-
-  it(`can handle a claim of ${amountOfTickets} tickets in batch sizes of ${ticketBatchSize}`, async () => {
-    const initialBalances = await getBalances(alice, bob, tokenContract);
-
-    USE_ERC20
-      ? await l1Contract.depositToken(tokenContract.address, depositValue)
-      : await l1Contract.depositEth({ value: depositValue });
-
-    const tickets: Ticket[] = [];
-    const ticketSignatures = [];
-    for (let i = 0; i < amountOfTickets; i++) {
-      const newTicket = {
-        senderNonce: i,
-        value: ticketValue,
-        receiver: alice.address,
-        sender: bob.address,
-        escrowHash: escrowHash,
-        expiry: ticketExpiry,
-        token: USE_ERC20 ? tokenContract.address : ETH_TOKEN_ADDRESS,
-      };
-
-      const ticketHash = hashTicket(newTicket);
-      const signature = await signData(ticketHash, bob.privateKey);
-
-      tickets.push(newTicket);
-      ticketSignatures.push(signature);
-    }
-    const preimages = new Array(amountOfTickets).fill(preimage);
-
-    for (
-      let i = ticketBatchSize;
-      i <= tickets.length;
-      i = i + ticketBatchSize
-    ) {
-      await l1Contract.claimTickets(
-        tickets.slice(i - ticketBatchSize, i),
-        preimages.slice(i - ticketBatchSize, i),
-        ticketSignatures.slice(i - ticketBatchSize, i)
+      const result = await l1Contract.claimTicket(tickets[i], preimage, {
+        r,
+        s,
+        v,
+      });
+      ticketGasUsage.gasUsed = ticketGasUsage.gasUsed.add(
+        (await result.wait()).gasUsed
       );
+      ticketGasUsage.claims++;
     }
-
     const finalBalances = await getBalances(alice, bob, tokenContract);
 
     const expectedTotalTransferred = BigNumber.from(
@@ -162,5 +152,71 @@ describe(`L1 Contract using ${USE_ERC20 ? "ERC20 tokens" : "ETH"}`, () => {
     );
 
     expect(expectedTotalTransferred.eq(actualTotalTransferred)).to.be.true;
+
+    gasPerScenario["sequential claims"] = ticketGasUsage;
   });
+  for (const ticketBatchSize of ticketBatchSizes) {
+    it(`can handle a claim of ${amountOfTickets} tickets in batch sizes of ${ticketBatchSize}`, async () => {
+      const ticketGasUsage = {
+        gasUsed: BigNumber.from(0),
+        claims: 0,
+        batchSize: ticketBatchSize,
+      };
+      const initialBalances = await getBalances(alice, bob, tokenContract);
+
+      USE_ERC20
+        ? await l1Contract.depositToken(tokenContract.address, depositValue)
+        : await l1Contract.depositEth({ value: depositValue });
+
+      const tickets: Ticket[] = [];
+      const ticketSignatures = [];
+      for (let i = 0; i < amountOfTickets; i++) {
+        const newTicket = {
+          senderNonce: i,
+          value: ticketValue,
+          receiver: alice.address,
+          sender: bob.address,
+          escrowHash: escrowHash,
+          expiry: ticketExpiry,
+          token: USE_ERC20 ? tokenContract.address : ETH_TOKEN_ADDRESS,
+        };
+
+        const ticketHash = hashTicket(newTicket);
+        const signature = await signData(ticketHash, bob.privateKey);
+
+        tickets.push(newTicket);
+        ticketSignatures.push(signature);
+      }
+      const preimages = new Array(amountOfTickets).fill(preimage);
+
+      for (
+        let i = ticketBatchSize;
+        i <= tickets.length;
+        i = i + ticketBatchSize
+      ) {
+        const result = await l1Contract.claimTickets(
+          tickets.slice(i - ticketBatchSize, i),
+          preimages.slice(i - ticketBatchSize, i),
+          ticketSignatures.slice(i - ticketBatchSize, i)
+        );
+
+        ticketGasUsage.gasUsed = ticketGasUsage.gasUsed.add(
+          (await result.wait()).gasUsed
+        );
+        ticketGasUsage.claims += ticketBatchSize;
+      }
+
+      const finalBalances = await getBalances(alice, bob, tokenContract);
+
+      const expectedTotalTransferred = BigNumber.from(
+        amountOfTickets * ticketValue
+      );
+      const actualTotalTransferred = finalBalances.alice.sub(
+        initialBalances.alice
+      );
+
+      expect(expectedTotalTransferred.eq(actualTotalTransferred)).to.be.true;
+      gasPerScenario[`batched claims ${ticketBatchSize}`] = ticketGasUsage;
+    });
+  }
 });
