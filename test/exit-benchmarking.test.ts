b0afde45eb8f29c2b774705829caa07b0062d330
Alex Gap <alex.gap@gmail.com>
2022-01-19 11:30:42 -0800

    DRY and reorg

--- test/exit-benchmarking.test.ts
+++ test/exit-benchmarking.test.ts
@@ -6,27 +6,35 @@ import { BigNumber } from "ethers";
 import { ethers } from "hardhat";
 
 import { TestToken__factory } from "../contract-types/factories/TestToken__factory";
-import { EntryChainTicketStruct } from "../contract-types/EntryChainEscrow";
-import { TicketsWithNonce } from "../src/types";
-import { hashTickets, signData } from "../src/utils";
 import {
+  authorizeWithdrawal,
   customerPK,
+  deposit,
   distributeExitChainTokens,
   ExitChainTestSetup,
   getOptimismL1Fee,
   lpPK,
   ScenarioGasUsage,
-  ticketToEntryChainTicket,
   waitForTx,
 } from "./utils";
 import { ExitChainEscrow__factory } from "../contract-types/factories/ExitChainEscrow__factory";
 import { SAFETY_DELAY } from "../src/constants";
-import { ExitChainDepositStruct } from "../contract-types/ExitChainEscrow";
 import Table from "cli-table";
 
 type ExitChainScenarioGasUsage = ScenarioGasUsage & {
   type: "depositOnExitChain" | "authorizeWithdrawal" | "claimExitChainFunds";
 };
+
+const gasLimit = 30_000_000;
+const tokenBalance = 1_000_000;
+
+const customerWallet = new ethers.Wallet(customerPK, ethers.provider);
+const lpWallet = new ethers.Wallet(lpPK, ethers.provider);
+
+const exitChainDeployer = new ExitChainEscrow__factory(lpWallet);
+const tokenDeployer = new TestToken__factory(lpWallet);
+let testSetup: ExitChainTestSetup;
+
 export function printExitScenarioGasUsage(
   scenarios: ExitChainScenarioGasUsage[]
 ) {
@@ -59,6 +67,7 @@ export function printExitScenarioGasUsage(
   }
   console.log(table.toString());
 }
+
 async function runScenario(
   batchSize: number,
   trustedNonce: number
@@ -67,35 +76,18 @@ async function runScenario(
 
   const trustedAmount = 10000;
 
-  const { customerWallet } = testSetup;
-
   let totalDepositGas = BigNumber.from(0);
   let totalDepositOptimismFee = BigNumber.from(0);
-  for (let i = 0; i < batchSize; i++) {
-    const depositAmount = 1;
-    const deposit: ExitChainDepositStruct = {
-      trustedNonce,
-      trustedAmount,
-      depositAmount,
-      entryChainRecipient: customerWallet.address,
-      token: testSetup.exitChainToken.address,
-    };
-    const transResult = await testSetup.customerExitChain
-      .connect(customerWallet)
-      .depositOnExitChain(deposit);
 
-    const { gasUsed } = await waitForTx(transResult);
-    if (i == 0 || i == batchSize - 1) {
-      console.log(
-        `Deposit ${i} gasUsed: ${gasUsed.toNumber()} l1Fee ${getOptimismL1Fee(
-          transResult
-        ).toNumber()}`
-      );
-    }
-    totalDepositGas = totalDepositGas.add(gasUsed);
-    totalDepositOptimismFee = totalDepositOptimismFee.add(
-      getOptimismL1Fee(transResult)
+  for (let i = 0; i < batchSize; i++) {
+    const { gasUsed, optimismL1Fee } = await deposit(
+      testSetup,
+      trustedNonce,
+      trustedAmount
     );
+
+    totalDepositGas = totalDepositGas.add(gasUsed);
+    totalDepositOptimismFee = totalDepositOptimismFee.add(optimismL1Fee);
   }
 
   results.push({
@@ -104,34 +96,22 @@ async function runScenario(
     totalGasUsed: totalDepositGas,
     optimismCost: totalDepositOptimismFee,
   });
+
   const { lpExitChain } = testSetup;
-
-  const tickets: EntryChainTicketStruct[] = [];
-  for (let i = 0; i < batchSize; i++) {
-    tickets.push(
-      ticketToEntryChainTicket(await lpExitChain.tickets(trustedNonce + i))
-    );
-  }
-
-  const ticketsWithNonce: TicketsWithNonce = {
-    startNonce: trustedNonce,
-    tickets,
-  };
-
-  const signature = signData(hashTickets(ticketsWithNonce), lpPK);
-  const authResult = await lpExitChain.authorizeWithdrawal(
+  const authorizeResults = await authorizeWithdrawal(
+    testSetup,
     trustedNonce,
-    trustedNonce + batchSize - 1,
-    signature
+    batchSize
   );
-  const { gasUsed } = await waitForTx(authResult);
+
   results.push({
     type: "authorizeWithdrawal",
     batchSize,
-    totalGasUsed: gasUsed,
-    optimismCost: getOptimismL1Fee(authResult),
+    totalGasUsed: authorizeResults.gasUsed,
+    optimismCost: authorizeResults.optimismL1Fee,
   });
   await ethers.provider.send("evm_increaseTime", [SAFETY_DELAY + 1]);
+
   const claimResult = await lpExitChain.claimExitChainFunds(trustedNonce);
   const claimReceipt = await waitForTx(claimResult);
   results.push({
@@ -143,15 +123,6 @@ async function runScenario(
   return results;
 }
 
-const gasLimit = 30_000_000;
-const tokenBalance = 1_000_000;
-
-const customerWallet = new ethers.Wallet(customerPK, ethers.provider);
-const lpWallet = new ethers.Wallet(lpPK, ethers.provider);
-
-const exitChainDeployer = new ExitChainEscrow__factory(lpWallet);
-const tokenDeployer = new TestToken__factory(lpWallet);
-let testSetup: ExitChainTestSetup;
 beforeEach(async () => {
   const exitChainToken = await tokenDeployer.deploy(tokenBalance);
   const entryChainTokenAddress = ethers.Wallet.createRandom().address;
@@ -186,9 +157,7 @@ beforeEach(async () => {
 const benchmarkResults: ExitChainScenarioGasUsage[] = [];
 it("gas benchmarking", async () => {
   let nonce = 0;
-  // The FIRST batch that is claimed on EntryChain incurs a write-to-zero-storage cost, which makes
-  // for a counter-intuitive list of results. So, we trigger an initial swap before
-  // starting the benchmark
+  // Perform an initial scenario run to
   await runScenario(1, nonce);
   nonce++;
   const benchmarkScenarios = [1, 2, 5, 20, 50, 100];
